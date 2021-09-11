176490478707554e9b9018496a3f04b2aa552ecf
stratonov555 <90403174+stratonov555@users.noreply.github.com>
2021-09-11 18:35:40 +0500

    Add files via upload

--- send.php
+++ send.php
@@ -1,7 +1,7 @@
 <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $to = "stratonov2017@gmail.com";    // Куда идет письмо
-        $from = "info@my-site.ru";    // От кого идет письмо
+        $from = "stratonov2017@gmail.com";    // От кого идет письмо
         $name = $_POST['name-at'];
         $email = $_POST['email-at'];
         $message = $_POST['message-at'];
